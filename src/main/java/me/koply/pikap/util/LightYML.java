cbb53f96b880bef6e3d753901a1e99222c9f09a8
koply <musafg@outlook.com>
2023-10-23 22:38:41 +0300

    changed: line length check

--- src/main/java/me/koply/pikap/util/LightYML.java
+++ src/main/java/me/koply/pikap/util/LightYML.java
@@ -16,7 +16,7 @@ public class LightYML extends HashMap<String, String> {
 
             String line;
             while ((line = reader.readLine()) != null) {
-                if (line.length() == 0 || line.isBlank() || line.startsWith("#")) continue;
+                if (line.isEmpty() || line.isBlank() || line.startsWith("#")) continue;
                 String[] sided = line.split(":");
                 this.put(sided[0].trim(), sided[1].trim());
             }
