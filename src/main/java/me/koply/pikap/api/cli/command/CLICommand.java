1710e162392667a6033e9c04e6235cafdd3dbc7c
koply <musafg@outlook.com>
2024-06-24 13:24:13 +0300

    added: prefixln()

--- src/main/java/me/koply/pikap/api/cli/command/CLICommand.java
+++ src/main/java/me/koply/pikap/api/cli/command/CLICommand.java
@@ -6,4 +6,8 @@ public interface CLICommand {
     default void println(String o) {
         Console.println(o);
     }
+
+    default void prefixln(String o) {
+        Console.prefixln(o);
+    }
 }
