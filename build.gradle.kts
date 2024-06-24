783ff673110f329bef60feb5f3079b788b11ec3c
Aki <mertunctuncer@gmail.com>
2024-06-24 22:11:01 +0300

    Added gradle.

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    java
+    `maven-publish`
+    application
+    alias(libs.plugins.com.github.johnrengelman.shadow) apply true
+}
+
+group = "me.koply"
+version = "1.0.0-alpha"
+description = "Pikap"
+
+dependencies {
+    implementation(libs.dev.arbjerg.lavaplayer)
+    implementation(libs.dev.lavalink.youtube.v2)
+    implementation(libs.org.slf4j.slf4j.simple)
+    implementation(libs.com.github.kwhat.jnativehook)
+    implementation(libs.com.github.tomas.langer.chalk)
+    implementation(libs.net.oneandone.reflections8.reflections8)
+    implementation(libs.com.github.jncrmx.discord.game.sdk4j)
+    implementation(libs.org.xerial.sqlite.jdbc)
+    implementation(libs.com.j256.ormlite.ormlite.core)
+    implementation(libs.com.j256.ormlite.ormlite.jdbc)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+application {
+    mainClass = "me.koply.pikap.Main"
+    applicationDefaultJvmArgs = listOf("-Dfile.encoding=UTF-8")
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks {
+    shadowJar {
+        minimize {
+            exclude(dependency(libs.org.xerial.sqlite.jdbc.get()))
+        }
+    }
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+}
